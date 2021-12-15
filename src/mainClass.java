a4dc1c4571f6e1e6ff06ef1944e824c065ad1d17
saltukozelgul <saltukozelgul@gmail.com>
2021-12-15 12:04:21 +0300

    Test

---
+++ src/mainClass.java
@@ -0,0 +1,9 @@
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		priceTaker.a101("8690526095202");
+
+	}
+
+}
---
+++ src/priceTaker.java
@@ -0,0 +1,31 @@
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class priceTaker {
+	
+	
+	
+	public static void a101(String barcode) {
+		System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.navigate().to("https://www.a101.com.tr/");
+		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/button[2]")).click();
+		WebElement searchBar = driver.findElement(By.name("search_text"));
+		searchBar.sendKeys(barcode);
+		driver.findElement(By.xpath("/html/body/section/header/div/div[2]/div[3]/form/button")).click();
+		String name = driver.findElement(By.className("name")).getText();
+		String price = driver.findElement(By.className("current")).getText();
+		System.out.println(name +": " + price);
+		driver.close();
+		}
+	
+}
