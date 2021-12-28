49a6e388975be4c2c1b3936930e2e39b1224e17a
Mert İnan Saygılı <mert71719601@gmail.com>
2021-12-28 14:43:35 +0300

    kkekek

--- src/Basket.java
+++ src/Basket.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Basket {
@@ -23,13 +29,46 @@ public class Basket {
 		return 0;
 	}
 	
-	public  int getProductCount() {
-		return 0;
+	public  int getProductCount(User current_user) {
+		// File variables
+		int index_f_email = current_user.getEmail().indexOf("."), product_count = 0;;
+		String file_name = ".\\users\\" + current_user.getEmail().substring(0, index_f_email) + ".txt", line = "";
+		try { // Opening and reading file
+			File file = new File(file_name);
+			BufferedReader read = new BufferedReader(new FileReader(file_name));
+			while((line = read.readLine()) != null) {
+				if(line.contains("Market Name: ")) {
+					product_count = product_count + 1; // with this loop we will find how many product we have
+				}
+			}
+			read.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		return product_count;
 	}
 	
 	// Functions
-	public void clear() {
-		
+	public void clear(User current_user) {
+		int index_f_email = current_user.getEmail().indexOf(".");
+		String file_name = ".\\users\\" + current_user.getEmail().substring(0, index_f_email) + ".txt", line = "", information = "";
+		String new_content = "";
+		try {
+			new_content = "E-mail: " + current_user.getEmail() + "\nPassowrd: " + current_user.getPassoword() + "\n";
+			
+			System.out.println(new_content);
+			FileWriter write_t_file = new FileWriter(file_name);
+			PrintWriter printWriter = new PrintWriter(write_t_file);
+			
+			printWriter.printf(new_content);
+			
+			write_t_file.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void addProduct(Product products) {
@@ -43,5 +82,6 @@ public class Basket {
 	public void addCoupon(String coupon_code) {
 		
 	}
+
 
 }
--- src/BasketPanel.java
+++ src/BasketPanel.java
@@ -27,46 +27,50 @@ public class BasketPanel {
 	private ArrayList<JLabel> label_ProductInformation = new ArrayList<>();
 	
 	// Global variables and objects\
-	private ArrayList<ArrayList<String>> productInformations = new ArrayList<ArrayList<String>>();
+	private ArrayList<String> productInformations = new ArrayList<String>();
 	private User current_user;
+	private Basket basket = new Basket();
+	private ArrayList<Product> products = new ArrayList<Product>();
 	
-	
-	public BasketPanel(User user) {
+	// Constructor
+	public BasketPanel(User current_user) {
 		
 		// Assigning of user class
-		this.current_user = user;
+		this.current_user = current_user;
 		
 		// File variables
-		int index_f_email = user.getEmail().indexOf(".");
-		String file_name = ".\\users\\" + user.getEmail().substring(0, index_f_email) + ".txt";
+		int index_f_email = current_user.getEmail().indexOf(".");
+		String file_name = ".\\users\\" + current_user.getEmail().substring(0, index_f_email) + ".txt";
 		
 		
 		// Calls frame settings
 		setFrameSettings();
 		
-		int product_count = productCount();
+		int product_count = productCount();	
 		
-		
-		
-		// if user clicks to the return button
+		// if user clicks to the return button MainPanel opens
 		button_Return.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				MainPanel mp = new MainPanel(user);
+				MainPanel mp = new MainPanel(current_user);
 				frame.dispose();
 				
 			}
 			
 		});
 		
-		// if user clicks to the Clear All button
+		// if user clicks to the remove all products on the user basket 
 		button_ClearAll.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				clear_All(user);
-				BasketPanel bp = new BasketPanel(user);
+				
+				// Calls clear function on basket class
+				basket.clear(current_user);
+				
+				
+				BasketPanel bp = new BasketPanel(current_user);
 				frame.dispose();
 				
 			}
@@ -74,7 +78,6 @@ public class BasketPanel {
 		});
 		
 	} // end of constructor
-	
 	
 	// Frame settings
 	public void setFrameSettings() {
@@ -155,14 +158,14 @@ public class BasketPanel {
 	
 	}
 	
-	// Label settings
+	// Label settings and Product, buradan devam etmeliyim
 	public void setLabelSettings() {
-		int product_count = productCount();
+		
+		int product_count = productCount(); 
 		
 		int x_coordinate = 5, y_coordinate = 90; // coordinates
 			
 		label_ProductInformation.add(new JLabel());
-			
 		// File variables
 		int index_f_email = this.current_user.getEmail().indexOf(".");
 		String file_name = ".\\users\\" + this.current_user.getEmail().substring(0, index_f_email) + ".txt", line = "", information = "";
@@ -173,18 +176,28 @@ public class BasketPanel {
 			BufferedReader read = new BufferedReader(new FileReader(file_name));
 			while((line = read.readLine()) != null) {
 				if(line.contains("Market Name: ")) {
+					
 					line.replace("\n", "");
 					information = line.replace("Market Name: ", "") + " ";
+					productInformations.add(line.replace("Market Name:", ""));
+					
 				}
 				else if(line.contains("Product Name: ")) {
+					
 					information = information + line.replace("Product Name: ", "") + " ";
+					productInformations.add(line.replace("Product Name: ", ""));
 				}
 				else if(line.contains("Product Price: ")) {
 					information = information + line.replace("Product Price: ", "") + "tl";
+					productInformations.add(line.replace("Product Price", ""));
 				}
 				else if(line.contains("Product No: ")); // just go
 				else{
 					if(information != "") {
+						
+						// Buraya urunun turune gore nesne olusturulup baskete gonderilmeli
+						// Product product = new Product(" ", Float.valueOf(productInformations.get(2)), productInformations.get(1), productInformations.get(0));
+						
 						informations.add(information);
 					}
 					information = ""; // clears information string
@@ -195,8 +208,6 @@ public class BasketPanel {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		System.out.println(informations);
 		
 		for(int i=0; i<product_count; i++) { // creates labels for every different product
 			
@@ -218,48 +229,10 @@ public class BasketPanel {
 	// Finds how many product that user has on his/her basket
 	public int productCount() {
 		
-		// File variables
-		int index_f_email = this.current_user.getEmail().indexOf("."), product_count = 0;;
-		String file_name = ".\\users\\" + this.current_user.getEmail().substring(0, index_f_email) + ".txt", line = "";
+		// returns product count
+		int productCount = basket.getProductCount(current_user);
 		
-		try { // Opening and reading file
-			File file = new File(file_name);
-			BufferedReader read = new BufferedReader(new FileReader(file_name));
-			while((line = read.readLine()) != null) {
-				if(line.contains("Market Name: ")) {
-					product_count = product_count + 1; // with this loop we will find how many product we have
-				}
-			}
-			read.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return product_count;
-	}
-	
-	// clears all items on basket
-	public void clear_All(User user) {
-		
-		int index_f_email = this.current_user.getEmail().indexOf(".");
-		String file_name = ".\\users\\" + this.current_user.getEmail().substring(0, index_f_email) + ".txt", line = "", information = "";
-		String new_content = "";
-		try {
-			new_content = "E-mail: " + user.getEmail() + "\nPassowrd: " + user.getPassoword() + "\n";
-			
-			System.out.println(new_content);
-			FileWriter write_t_file = new FileWriter(file_name);
-			PrintWriter printWriter = new PrintWriter(write_t_file);
-			
-			printWriter.printf(new_content);
-			
-			write_t_file.close();
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		return productCount;
 	}
 	
 } // end of BasketPanel class
