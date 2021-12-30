c3d95c37c9b2f0abbac1e902ee2604b4738ea7e5
saltukozelgul <saltukozelgul@gmail.com>
2021-12-30 13:03:23 +0300

    Exception handling added.

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
 	// Main Class of Project
 	
 	public static void main(String[] args) {
-		User user = new User();
-		user.setEmail("mert71719601@gmail.com");
-		user.setPassword("MerhabaMert");
+		//User user = new User();
+		//user.setEmail("mert71719601@gmail.com");
+		//user.setPassword("MerhabaMert");
 		//LogInSignUp LISU = new LogInSignUp();
-		MainPanel mp = new MainPanel(user);
+		//MainPanel mp = new MainPanel(user);
 		//BarcodeReader br = new BarcodeReader();
 		//priceTaker.trendyol("8690526095226");
-		//priceTaker.a101("8690526095226");
+		priceTaker.a101("86905260956");
 		//priceTaker.carrefour("8690526095226");
 		//BasketPanel bp = new BasketPanel(user);
 	}
--- src/priceTaker.java
+++ src/priceTaker.java
@@ -20,84 +20,115 @@ public class priceTaker {
 	}
 	
 	public static ArrayList<String> a101(String barcode) {
-		
 		ArrayList<String> info = new ArrayList<String>();
-		
-		WebDriver driver = createDriver();
-		
-		driver.navigate().to("https://www.a101.com.tr/list/?search_text="+barcode);
-		
-		String name = driver.findElement(By.className("name")).getText();
-		String price = driver.findElement(By.className("current")).getText();
-		
-		price = price.replace(',', '.');
-		price = price.split(" ")[0];
-		
-		driver.findElement(By.className("name-price")).click();
-		
-		List<WebElement> list = driver.findElements(By.className("breadcrumb-item"));
-		String type = list.get(list.size() - 2).getText();
-		
-		
-		info.add("A101"); info.add(name); info.add(price); info.add(type); 
-		
-		System.out.print(info);
-		driver.quit();
-		return info;
+		try {
+			
+			WebDriver driver = createDriver();
+			
+			driver.navigate().to("https://www.a101.com.tr/list/?search_text="+barcode);
+			
+			String name = driver.findElement(By.className("name")).getText();
+			String price = driver.findElement(By.className("current")).getText();
+			
+			price = price.replace(',', '.');
+			price = price.split(" ")[0];
+			
+			driver.findElement(By.className("name-price")).click();
+			
+			List<WebElement> list = driver.findElements(By.className("breadcrumb-item"));
+			String type = list.get(list.size() - 2).getText();
+			
+			
+			info.add("A101"); info.add(name); info.add(price); info.add(type); 
+			
+			System.out.print(info);
+			driver.quit();
+			return info;
+		}
+		catch (Exception e) {
+			System.out.println("There was an error while findind product");
+		}
+		return null;
 	}
 	
 	public static ArrayList<String> carrefour(String barcode) {
-		
 		ArrayList<String> info = new ArrayList<String>();
-		
-		WebDriver driver = createDriver();
-		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.navigate().to("https://www.carrefoursa.com/search/?text="+barcode);
-		
-		String price = driver.findElement(By.className("price-cont")).getText();
-		String name = driver.findElement(By.className("item-name")).getText();
-				
-		price = price.replace(',', '.');
-		price = price.split(" ")[0];
+		try {
+			WebDriver driver = createDriver();
+			
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.navigate().to("https://www.carrefoursa.com/search/?text="+barcode);
+			
+			String price = driver.findElement(By.className("price-cont")).getText();
+			String name = driver.findElement(By.className("item-name")).getText();
+					
+			price = price.replace(',', '.');
+			price = price.split(" ")[0];
 
-		
-		info.add("CARREFOUR"); info.add(name); info.add(price); 
-		
-		System.out.println(info);
-		
-		driver.quit();
-		
-		return info;
+			
+			info.add("CARREFOUR"); info.add(name); info.add(price); 
+			
+			System.out.println(info);
+			
+			driver.quit();
+			
+			return info;
+		}
+		catch (Exception e) {
+			System.out.println("There was an error while findind product");
+		}
+		return null;
 	}
 	
-	public static void amazon(String barcode) {
-	
+	public static ArrayList<String> amazon(String barcode) {
+		ArrayList<String> info = new ArrayList<String>();
+		try {
+			
+		}
+		catch (Exception e) {
+			System.out.println("There was an error while finding product");
+		}
+		return null;
 	}
 	
 	public static ArrayList<String> trendyol(String barcode) {
 		
 		ArrayList<String> info = new ArrayList<String>();
 		
-		WebDriver driver = createDriver();
+		try {
+			WebDriver driver = createDriver();
+			
+			driver.navigate().to("https://www.trendyol.com/sr?q="+barcode);
+			
+			String brand = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[3]/div/div/div[1]/a/div[2]/div[1]/div/span[1]")).getText();
+			String name = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[3]/div/div/div[1]/a/div[2]/div[1]/div/span[2]")).getText();
+			String price = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[3]/div/div/div[1]/a/div[2]/div[3]/div/div/div[3]/div[2]")).getText();
+			
+			info.add("TRENDYOL"); info.add(brand); info.add(name); info.add(price); 
+			
+			System.out.print(info);
+			
+			driver.quit();
+			
+			return info;
+		}
+		catch (Exception e) {
+			System.out.println("There was an error while finding product");
+		}
+		return null;
 		
-		driver.navigate().to("https://www.trendyol.com/sr?q="+barcode);
-		
-		String brand = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[3]/div/div/div[1]/a/div[2]/div[1]/div/span[1]")).getText();
-		String name = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[3]/div/div/div[1]/a/div[2]/div[1]/div/span[2]")).getText();
-		String price = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[3]/div/div/div[1]/a/div[2]/div[3]/div/div/div[3]/div[2]")).getText();
-		
-		info.add("TRENDYOL"); info.add(brand); info.add(name); info.add(price); 
-		
-		System.out.print(info);
-		
-		driver.quit();
-		
-		return info;
 	}
 	
-	public static void hepsiburada(String barcode) {
+	public static ArrayList<String> hepsiburada(String barcode) {
+		ArrayList<String> info = new ArrayList<String>();
 		
+		try {
+			
+		}
+		catch (Exception e) {
+			System.out.println("There was an error while finding product");
+		}
+		return null;
 	}
 	
 }
