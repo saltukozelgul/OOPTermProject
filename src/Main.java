72f64b483440943c45c964627bd55434a63767cb
Mert İnan Saygılı <mert71719601@gmail.com>
2022-02-12 01:33:44 +0300

    Bir sey baktim

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 	// Main Class of Project
 	
 	public static void main(String[] args) {
-		LogInSignUp login = new LogInSignUp();
+		//LogInSignUp login = new LogInSignUp();
 		
 		User user1 = new User();
 		user1.setEmail("mert71719601@gmail.com");
